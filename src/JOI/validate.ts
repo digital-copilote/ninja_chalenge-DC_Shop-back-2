7efed254ac6e3195849e63e67c4bef6cf0b2ebb7
marie5060 <marie5060hotmail.com>
2022-02-21 15:49:16 +0100

    shiefls color added in resolvers/typesdefs/helpers/joi for theme

--- src/JOI/validate.ts
+++ src/JOI/validate.ts
@@ -55,6 +55,7 @@ export const size = Joi.object().keys({
 
 export const theme = Joi.object().keys({
   name: Joi.string().min(3).required(),
+  color: Joi.string().min(6).required(),
 });
 
 export const shirt = Joi.object().keys({
